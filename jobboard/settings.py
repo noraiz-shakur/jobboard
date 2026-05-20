31b93da340aac9fd24b6a1bd6cc8175bb778ee10
Noraiz Shakur <noraizahmad128@gmail.com>
2026-05-20 13:00:45 +0500

    3rd commit

--- jobboard/settings.py
+++ jobboard/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'jobboard.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
